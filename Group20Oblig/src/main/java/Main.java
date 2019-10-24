a6126e084987c63ecd19e62c036ec051ef6c750c
Leo Barzinje <Mohammnb@hiof.no>
2019-10-24 12:41:32 +1000

    endret på deeznutz

---
+++ Group20Oblig/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public Main(){
+        String deeznutz = "deez";
+        String lol = "eerere";
+        System.out.println(deeznutz);
+        System.out.println(lol);
+        
+    }
+}
