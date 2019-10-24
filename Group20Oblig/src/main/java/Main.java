0b114f945b8cac1e67ab4955adb4295d4ec89865
Leo Barzinje <Mohammnb@hiof.no>
2019-10-24 12:46:05 +1000

    Test av github er fullført, starter på nytt nå.

--- Group20Oblig/src/main/java/Main.java
+++ Group20Oblig/src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
 
     public Main(){
-        String deeznutz = "deez";
-        String lol = "eerere";
-        System.out.println(deeznutz);
-        System.out.println(lol);
         
     }
 }
